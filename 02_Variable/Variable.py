1a678e4c78a8f65edf59721495407f9d123bd46d
lingann <liangnn.m@gmail.com>
2019-04-21 20:13:52 +0800

    完善Statements.py

--- 02_Variable/Variable.py
+++ 02_Variable/Variable.py
@@ -1,7 +1,8 @@
 num1 = 10
 num2 = 20
 string = "Hello World"
+isTrue = True
 
 print(num1 + num2)
+print(isTrue)
 print(string)
-
---
+++ 03_Statements/Statements.py
@@ -0,0 +1,21 @@
+# 条件
+num1 = 53
+num2 = 10
+
+if num1 < num2:
+    print("return ", num1)
+else:
+    print("return ", num2)
+
+# 循环
+persons = [
+    "老子",
+    "庄子",
+    "墨子",
+    "孔子"
+]
+
+for person in persons:
+    print(person)
+
+
